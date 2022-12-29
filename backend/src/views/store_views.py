ed67080f8d7e508226a233f3ce7213dbd3b0d23b
rajat-gith <rajatnayak1582002@gmail.com>
2022-12-29 23:09:52 +0530

    Implemented Search Func

--- backend/src/views/store_views.py
+++ backend/src/views/store_views.py
@@ -7,36 +7,44 @@ from src.serializer import *
 
 @api_view(['GET'])
 def getStores(request):
-    queryset=stores.objects.all()
-    serializer=StoreSerializer(queryset,many=True)
+    query = request.query_params.get('keyword')
+    if (query == None):
+        query = ''
+    queryset = stores.objects.filter(name__icontains=query)
+    serializer = StoreSerializer(queryset, many=True)
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 def getStoreOwners(request):
-    owners=store_owner.objects.all()
-    serializer=StoreOwner_Serializer(owners,many=True)
+
+    owners = store_owner.objects.all()
+    serializer = StoreOwner_Serializer(owners, many=True)
     return Response(serializer.data)
 
-@api_view(['GET'])
-def getStoreById(request,pk):
-    store=stores.objects.get(_id=pk)
-    serializer=StoreSerializer(store,many=False)
-    return Response(serializer.data)
 
 @api_view(['GET'])
-def getStoreOwnerById(request,pk):
-    owner=store_owner.objects.get(_id=pk)
-    serializers=StoreOwner_Serializer(owner,many=False)
+def getStoreById(request, pk):
+    store = stores.objects.get(_id=pk)
+    serializer = StoreSerializer(store, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getStoreOwnerById(request, pk):
+    owner = store_owner.objects.get(_id=pk)
+    serializers = StoreOwner_Serializer(owner, many=False)
     return Response(serializers.data)
 
+
 @api_view(['POST'])
-def createStoreBrand(request,pk):
-    store=stores.objects.get(_id=pk)
+def createStoreBrand(request, pk):
+    store = stores.objects.get(_id=pk)
     data = request.data
-    Brand=brand.object.create(
+    Brand = Product_brand.object.create(
         name=data['name'],
         quantity=data['quantity'],
         status=data['status']
     )
 
-    brands=stores.brand_set.all()
+    brands = stores.brand_set.all()
